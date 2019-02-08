7e3460e9123154394a5fc0f397f14bb2c066398d
Tim Sutherland <43186483+Timsutherland4@users.noreply.github.com>
2019-02-08 11:24:23 -0500

    Delete appTim.js

--- appTim.js
+++
@@ -1,9 +0,0 @@
-// from data.js
-var tableData = dataTim;
-
-// YOUR CODE HERE!
-// Get references to the tbody element, input field and button
-var $tbody = document.querySelector('tbody');
-var $stateInput = document.querySelector('#state');
-var $searchBtn = document.querySelector('#search');
-
